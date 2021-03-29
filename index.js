cbc6657f05e2dc8be544f22dca9fbf2e613ae381
Sahinur Islam <infosahinur@gmail.com>
2021-03-29 23:47:17 +0600

    new

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ const express = require('express');
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send("I know hot to open Node... YAY!!")
-})
+const users = ["Sahinur", "Arif", "Mim", "Kolamit", "Hasmot"]
 
-app.listen(4000, () => console.log('listening port: 3000'));
+// single api
+app.get('/', (req, res) => {
+    const fruit ={
+        products: 'ada',
+        price: 225
+    }
+    res.send(fruit);
+});
+// route api or parameters api
+app.get('/fruits/banana', (req, res) => {
+    res.send({fruit: 'banana', quantity:1000, price:100000});
+});
+
+// dynamic api
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const name = users[id];
+res.send({id, name});
+});
+
+app.listen(4000, () => console.log('listening port: 4000'));
